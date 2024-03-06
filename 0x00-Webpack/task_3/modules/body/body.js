6af23dc302db64de8ab32ab879a7d986be9f0afa
sharonkisenga <sharonglor69@gmail.com>
2024-03-06 03:19:25 -0800

    pack

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,18 @@
+"Using the folder named task_3";
+const $ = require("jquery");
+const _ = require("lodash");
+import "./body.css";
+
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+
+function updateCounter() {
+  let clicks = $("#count").html() || 0;
+  $("button").on("click", () => {
+    clicks++;
+    $("#count").html(`${clicks} clicks on the button`);
+  });
+}
+
+_.debounce(updateCounter(), 500);
---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,6 @@
+"Using the folder named task_3";
+const $ = require("jquery");
+import "./footer.css";
+
+$("body").append("<footer></footer>");
+$("footer").append("<p>Copyright - Holberton School</p>");
